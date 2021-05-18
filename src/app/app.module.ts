e6e811ffa7003342cecd748137b580487968f994
meriamgaied9 <meriamgaied9@gmail.com>
2021-05-18 09:58:45 +0100

    this  is for learning purposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { TestComponent } from './testComponent/testComponenet.component';
-import { TrialComponent } from './trial/trial.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent, ServersComponent, TestComponent, TrialComponent
-  ],
+    AppComponent, ServerComponent, ServersComponent],
   imports: [
     BrowserModule, 
     FormsModule  
--- src/app/testComponent/testComponenet.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component( {
-    selector : 'test-comp',
-    templateUrl: './testComponent.component.html'
-}) 
-export class TestComponent {
-
-}
